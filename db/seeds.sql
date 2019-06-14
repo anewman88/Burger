914d9a46723c25061356a11cd6efe9ca3cb0d685
Ann Newman <anewman1822@gmail.com>
2019-06-13 22:18:13 -0500

    created database file; updated server.js

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('beef', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('vegan', false);
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());  // parse application/json
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
